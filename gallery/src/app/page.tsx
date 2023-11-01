ef5cb4e5b43885fe1c0da631383c800316c2fd8c
mohamadZareai 	☾ <mohamadzareai@gmail.com>
2023-11-02 02:18:36 +0330

    🐛 fix text bux "

--- gallery/src/app/page.tsx
+++ gallery/src/app/page.tsx
@@ -6,11 +6,11 @@ export default function Home() {
       <div className="mx-auto max-w-7xl py-24 sm:px-6 sm:py-32 lg:px-8">
         <div className="relative isolate overflow-hidden bg-gray-900 px-6 py-24 text-center shadow-2xl sm:rounded-3xl sm:px-16">
           <h2 className="mx-auto max-w-2xl text-3xl font-bold tracking-tight text-white sm:text-4xl">
-            Welcome to Radshodam Gallery!
+           {` Welcome to Radshodam Gallery!`}
           </h2>
           <p className="mx-auto mt-6 max-w-xl text-lg leading-8 text-gray-300">
-            Radshodam Gallery" is your destination for a delightful visual
-            experience....
+           {` Radshodam Gallery" is your destination for a delightful visual
+            experience....`}
           </p>
 
           <svg
