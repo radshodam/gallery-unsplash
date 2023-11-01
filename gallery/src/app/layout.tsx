417e52f5977d39fc9482ed2bc6be78cb3c91d8fd
mohamadZareai 	☾ <mohamadzareai@gmail.com>
2023-11-01 16:38:36 +0330

    🎉 cra project

---
+++ gallery/src/app/layout.tsx
@@ -0,0 +1,18 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "NextJS 13.4 Image Gallery",
+  description: "Tutorial project by Radshodam",
+};
+
+export default function RootLayout({ children }: { children: React.ReactNode }) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>{children}</body>
+    </html>
+  );
+}
